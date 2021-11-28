6d16cc2b691abe8db597edb21680d227a5e28253
Abhay Sharma <avay.abhay@gmail.com>
2021-11-28 16:58:19 +0530

    logout completed

--- router.js
+++ router.js
@@ -13,10 +13,32 @@ router.post("/login", (req, res) => {
     req.body.password == credentials.password
   ) {
     req.session.user = req.body.email;
-    res.end("Welcome to the DashBoard!");
+    // res.end("Welcome to the DashBoard!");
+    res.redirect("/route/dashboard");
   } else {
     res.send("Invalid email or password");
   }
+});
+
+router.get("/dashboard", (req, res) => {
+  if (req.session.user) {
+    res.render("dashboard", { user: req.session.user });
+  } else {
+    res.end("UnAuthorized User");
+  }
+});
+
+router.get("/logout", (req, res) => {
+  req.session.destroy((err) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.render("base", {
+        title: "Logged Out",
+        logout: "You have successfully logged out!",
+      });
+    }
+  });
 });
 
 module.exports = router;
